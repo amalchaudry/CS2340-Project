624f089b03b4c9c1114c402fd53a717dc55f4ab8
amalchaudry <achaudry6@gatech.edu>
2022-02-19 18:52:35 -0500

    welcome screen

--- ODam/app/src/main/java/com/example/odam/MainActivity.java
+++ ODam/app/src/main/java/com/example/odam/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.odam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
