471a3b8dc7760df0b1e137e7111c89817630b7b8
Charlie Liu <cliu629@gatech.edu>
2022-02-19 23:17:02 -0500

    fixed welcome screen merge

--- ODam/app/src/main/java/com/example/odam/MainActivity.java
+++ ODam/app/src/main/java/com/example/odam/MainActivity.java
@@ -1,25 +1,20 @@
 package com.example.odam;
 
 import androidx.appcompat.app.AppCompatActivity;
-HEAD
 import android.view.View;
 
 
 import android.graphics.Color;
 import android.widget.Button;
- Amal
 import android.os.Bundle;
 
 import com.example.odam.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-HEAD
 
     private ActivityMainBinding binding;
 
-
-    Button button;
- Amal
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
