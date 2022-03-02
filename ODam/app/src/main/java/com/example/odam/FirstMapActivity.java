1ed67192079c839bfb8cd2c519f7e72b439b6891
Charlie Liu <cliu629@gatech.edu>
2022-03-01 19:06:29 -0500

    can place fishermen

--- ODam/app/src/main/java/com/example/odam/FirstMapActivity.java
+++ ODam/app/src/main/java/com/example/odam/FirstMapActivity.java
@@ -1,7 +1,14 @@
 package com.example.odam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -17,6 +24,9 @@ public class FirstMapActivity extends AppCompatActivity {
     private ImageView chosenTowerImage;
     private Difficulty diff;
     private boolean isPlacingTower = false;
+    private Bitmap bitmap;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +37,8 @@ public class FirstMapActivity extends AppCompatActivity {
 
         //change this to be equivalent to other configuration screen's difficulty variable
         chosenTowerImage = new ImageView(FirstMapActivity.this);
+        binding.getRoot().addView(chosenTowerImage);
+        bitmap = drawableToBitmap(binding.mapImage.getDrawable());
 
         diff = ((GameApplication) getApplication()).getDiff();
         Log.d("Diff", diff.toString());
@@ -53,8 +65,7 @@ public class FirstMapActivity extends AppCompatActivity {
         //testing
         binding.fisherButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                chosenTower = new FishermanTower(diff);
-                chosenTowerImage.setImageResource(chosenTower.getImage()); //TODO: Change to appropriate image
+                chooseNewTower(new FishermanTower(diff));
                 isPlacingTower = true;
             }
         });
@@ -62,23 +73,87 @@ public class FirstMapActivity extends AppCompatActivity {
         //TODO: Try drag and drop listener instead
         binding.mapImage.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
-                System.out.println("Hello");
                 chosenTowerImage.bringToFront();
                 int action = event.getAction();
                 switch (action) {
                 case MotionEvent.ACTION_DOWN:
-                    chosenTowerImage.setX(event.getX());
-                    chosenTowerImage.setY(event.getY());
+                    if (isPlacingTower && chosenTower != null) {
+                        chosenTowerImage.setImageResource(chosenTower.getImage());
+                        chosenTowerImage.setX((int)event.getX() - chosenTowerImage.getWidth() / 2);
+                        chosenTowerImage.setY((int)event.getY() - chosenTowerImage.getHeight());
+                        chosenTowerImage.setAlpha(0.5f);
+                    }
+                    break;
                 case MotionEvent.ACTION_MOVE:
-                    chosenTowerImage.setX(event.getX());
-                    System.out.println(chosenTowerImage.getX());
-                    chosenTowerImage.setY(event.getY());
-                case MotionEvent.ACTION_UP:
-                    chosenTowerImage.setX(event.getX());
-                    chosenTowerImage.setY(event.getY());
+                    if (isPlacingTower && chosenTower != null) {
+                        chosenTowerImage.setX((int)event.getX() - chosenTowerImage.getWidth() / 2);
+                        chosenTowerImage.setY((int)event.getY() - chosenTowerImage.getHeight());
+                    }
+                    break;
+                case MotionEvent.ACTION_UP://TODO: https://stackoverflow.com/questions/17931816/how-to-tell-if-an-x-and-y-coordinate-are-inside-my-button for bounds check
+                    if (isPlacingTower) {
+                        isPlacingTower = false;
+                        chosenTowerImage.setAlpha(1f);
+                        int selectedArea = bitmap.getPixel((int)event.getX(), (int)event.getY());
+                        binding.testview.setText(String.format("0x%08X", selectedArea));
+                        if (selectedArea == 0xff18bbc0) {
+                            chosenTowerImage.setImageResource(0);
+                        }
+                    }
+                    break;
                 }
                 return true;
             }
         });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public void chooseNewTower(Tower tower) {
+        chosenTower = tower;
+        chosenTowerImage = new ImageView(FirstMapActivity.this);
+        chosenTowerImage.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                case MotionEvent.ACTION_UP:
+                    display(tower, chosenTowerImage); //TODO: This will be the code that displays the tower's stats on press
+                    break;
+                }
+                return true;
+            }
+        });
+        binding.getRoot().addView(chosenTowerImage);
+        chosenTowerImage.setAdjustViewBounds(true);
+        chosenTowerImage.setMaxHeight(100);
+        chosenTowerImage.setMaxWidth(100);
+        chosenTowerImage.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+        chosenTowerImage.setDrawingCacheEnabled(true);
+    }
+
+    public void display(Tower tower, ImageView image) {
+        image.setX(200);
+        image.setY(200);
+    }
+
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 }
