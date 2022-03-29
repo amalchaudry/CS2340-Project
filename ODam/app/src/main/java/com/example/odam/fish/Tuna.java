23700ffcbe51779529317930821bb0a1e55cfabd
Charlie Liu <charlieliu2222@gmail.com>
2022-03-29 15:08:38 -0400

    no errors in fish

--- ODam/app/src/main/java/com/example/odam/fish/Tuna.java
+++ ODam/app/src/main/java/com/example/odam/fish/Tuna.java
@@ -2,18 +2,29 @@ package com.example.odam.fish;
 
 public class Tuna implements Fish {
     @Override public int getCoord () {
-
+        return 0;
     }
 
     @Override public int setCoord() {
-
+        return 0;
     }
 
     @Override public int getHealth () {
-
+        return 0;
     }
 
     @Override public int setHealth() {
+        return 0;
+    }
 
+
+    @Override
+    public void swim() {
+        return;
+    }
+
+    @Override
+    public void attackMon() {
+        return;
     }
 }
