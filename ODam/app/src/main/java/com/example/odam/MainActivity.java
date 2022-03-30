6047a117ab4305519e0839642da734adc39d446e
Amal <achaudry6@gatech.edu>
2022-03-29 23:14:06 -0400

    game over screen

--- ODam/app/src/main/java/com/example/odam/MainActivity.java
+++ ODam/app/src/main/java/com/example/odam/MainActivity.java
@@ -1,25 +1,19 @@
 package com.example.odam;
 
 import androidx.appcompat.app.AppCompatActivity;
-HEAD
 import android.view.View;
 
 
 import android.graphics.Color;
 import android.widget.Button;
- Amal
 import android.os.Bundle;
 
 import com.example.odam.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-HEAD
 
     private ActivityMainBinding binding;
-
-
     Button button;
- Amal
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
